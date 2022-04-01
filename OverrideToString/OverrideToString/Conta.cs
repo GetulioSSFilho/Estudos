f1e7a0deeb8dbdcbb9f802cbe391f189d59f12a0
Getulio Sergio <getuliosergio@hotmail.com>
2022-03-31 22:25:23 -0300

    Teste de Override do ToString() de uma Classe. Executar com Ctrl + F5

---
+++ OverrideToString/OverrideToString/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideToString
+{
+    class Conta
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia;
+        }
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture)
+                ;
+        }
+    }
+}
---
+++ OverrideToString/OverrideToString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+            Console.WriteLine("Depósito Inicial:");
+            conta = new Conta(27012, "Getulio Sergio", 9500.00);
+            Console.WriteLine(conta.ToString());
+
+            Console.WriteLine("Saldo Inicial + Depósito de R$5200:");
+            conta.Deposito(5200);
+            Console.WriteLine(conta.ToString());
+
+            Console.WriteLine("Saldo Anterior - Saque de R$700:");
+            conta.Saque(700);
+            Console.WriteLine(conta.ToString());
+        }
+    }
+}
